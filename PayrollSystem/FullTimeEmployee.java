b3f3a7bb233957cba4a4491b9be37fd860ba06bd
Nur Mohammed <mohammed2305101902@diu.edu.bd>
2025-03-14 12:00:30 +0600

    FullTimeEmployee.java
    For adding employee type

--- PayrollSystem/FullTimeEmployee.java
+++ PayrollSystem/FullTimeEmployee.java
@@ -4,10 +4,17 @@ public class FullTimeEmployee extends Employee {
     public FullTimeEmployee(String name, String id, double salary, double bonus) {
         super(name, id, salary);
         this.bonus = bonus;
+
+    
     }
 
     @Override
     public double calculateSalary() {
         return salary + bonus;
     }
+    @Override
+    public String EmployeeType(){
+        return "Full Time";
+    }
+    
 }
